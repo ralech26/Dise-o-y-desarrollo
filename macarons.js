243923b6e48b57318c292bec162dd4148d0ac7e5
ralech26 <107942707+ralech26@users.noreply.github.com>
2022-06-28 15:27:43 -0300

    Update macarons.js

--- macarons.js
+++ macarons.js
@@ -1,5 +1,35 @@
-let app = document.getElementById('typewriter');
- 
+const btnEnviar = document.getElementById('btn-enviar');
+
+const validación = (e) => {
+  e.preventDefault();
+  const nombreDeUsuario = document.getElementById('usuario');
+  const direcciónEmail = document.getElementById('email');
+  if (usuario.value === "") {
+    alert("Por favor, escribe tu nombre de usuario.");
+    usuario.focus();
+    return false;
+  }
+    
+  if (email.value === "") {
+    alert("Por favor, escribe tu correo electrónico");
+    email.focus();
+    return false;
+  }
+
+  if (!emailVálido(email.value)) {
+    alert("Por favor, escribe un correo electrónico válido");
+    emailAddress.focus();
+    return false;
+  }
+  
+  return true; //Se pueden enviar los datos del formulario al servidor
+}
+
+const emailVálido = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
